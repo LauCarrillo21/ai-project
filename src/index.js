af5b68b75813cf51a8837e186b88c17155ce5df0
LauCarrillo21 <laucarrillo21@gmail.com>
2024-12-13 12:00:15 -0600

    Added the AI API Integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-function generatePoem(event) {
-    event.preventDefault();
-  
+function displayPoem(response) {
+    console.log("poem generated");
+
     new Typewriter("#poem", {
-      strings: "La tombe dit à la rose",
+      strings: response.data.answer,
       autoStart: true,
       delay: 1,
-      cursor: "",
     });
+  }
+
+
+function generatePoem(event) {
+    event.preventDefault();
+
+    let instructionsInput = document.querySelector("#user-instructions");
+    let apiKey = "4bfb73a00210a185tacffd8o47774b6a";
+    let context = "Act as a funny AI-assistant that tells sweet and funny jokes";
+    let prompt = `Generate a short and unique joke about ${instructionsInput.value}`;
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    console.log(`Prompt = ${prompt}`);
+    console.log(`Context = ${context}`);
+
+    axios.get(apiUrl).then(displayPoem);
   }
   
   let poemFormElement = document.querySelector("#poem-generator-form");
